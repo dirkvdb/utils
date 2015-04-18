f74aedd9a2c0afc431d0f4e3a17d5b518ad92609
Dirk Vanden Boer <dirk.vdb@gmail.com>
2015-04-18 20:37:57 +0200

    Travis: use gcov-4.9

--- CMakeModules/CodeCoverage.cmake
+++ CMakeModules/CodeCoverage.cmake
@@ -60,19 +60,19 @@ ENDIF() # NOT CMAKE_COMPILER_IS_GNUCXX
 SET(CMAKE_CXX_FLAGS_COVERAGE
     "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
     CACHE STRING "Flags used by the C++ compiler during coverage builds."
-    FORCE PARENT_SCOPE)
+    FORCE)
 SET(CMAKE_C_FLAGS_COVERAGE
     "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
     CACHE STRING "Flags used by the C compiler during coverage builds."
-    FORCE PARENT_SCOPE)
+    FORCE)
 SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE
     ""
     CACHE STRING "Flags used for linking binaries during coverage builds."
-    FORCE PARENT_SCOPE)
+    FORCE)
 SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
     ""
     CACHE STRING "Flags used by the shared libraries linker during coverage builds."
-    FORCE PARENT_SCOPE)
+    FORCE)
 MARK_AS_ADVANCED(
     CMAKE_CXX_FLAGS_COVERAGE
     CMAKE_C_FLAGS_COVERAGE
